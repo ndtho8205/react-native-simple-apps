1ef8fef6c7a91003d2a368e16dc46892adf22082
Tho Nguyen Duc <ndtho8205@gmail.com>
2018-02-09 16:22:51 +0700

    Initial hackernews project.

---
+++ hackernews/src/App.js
@@ -0,0 +1,25 @@
+/**
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
